36677e6ddc26a5004f1eabd68bc15cf6e2ce322a
teukumunawark <munawartmk@gmail.com>
2023-02-19 11:49:12 +0700

    adding model

---
+++ lib/data/models/coord.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:pawang_hujan/domain/entity/coord_entity.dart';
+
+class Coord extends Equatable {
+  final double? lon;
+  final double? lat;
+
+  const Coord({this.lon, this.lat});
+
+  factory Coord.fromJson(Map<String, dynamic> json) => Coord(
+        lon: (json['lon'] as num?)?.toDouble(),
+        lat: (json['lat'] as num?)?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'lon': lon,
+        'lat': lat,
+      };
+
+  CoordEntity toEntity() => CoordEntity(lon: lon, lat: lat);
+
+  @override
+  List<Object?> get props => [lon, lat];
+}
