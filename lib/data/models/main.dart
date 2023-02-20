affec11c4e74a672306983c4794486e28fbc3611
teukumunawark <munawartmk@gmail.com>
2023-02-20 20:42:31 +0700

    update model

--- lib/data/models/main.dart
+++ lib/data/models/main.dart
@@ -25,10 +25,10 @@ class Main extends Equatable {
 
   factory Main.fromJson(Map<String, dynamic> json) => Main(
         temp: (json['temp'] as num?)?.toDouble(),
-        feelsLike: (json['feels_like'] as num?)?.toDouble(),
-        tempMin: (json['temp_min'] as num?)?.toDouble(),
-        tempMax: (json['temp_max'] as num?)?.toDouble(),
-        pressure: json['pressure'] as int?,
+        feelsLike: (json['feels_like'] as double?),
+        tempMin: (json['temp_min'] as double?),
+        tempMax: (json['temp_max'] as double?),
+        pressure: (json['pressure'] as int?),
         humidity: json['humidity'] as int?,
         seaLevel: json['sea_level'] as int?,
         grndLevel: json['grnd_level'] as int?,
