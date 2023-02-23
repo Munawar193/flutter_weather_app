145632a8bda3b11200204ee062ff029750cdef28
teukumunawark <munawartmk@gmail.com>
2023-02-23 23:50:12 +0700

    adding bloc

---
+++ lib/presentation/bloc/weather_bloc_state.dart
@@ -0,0 +1,30 @@
+part of 'weather_bloc_bloc.dart';
+
+abstract class WeatherBlocState extends Equatable {
+  const WeatherBlocState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherEmpty extends WeatherBlocState {}
+
+class WeatherLoading extends WeatherBlocState {}
+
+class WeatherError extends WeatherBlocState {
+  final String message;
+
+  const WeatherError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class WeatherHasData extends WeatherBlocState {
+  final WeathersEntity data;
+
+  const WeatherHasData(this.data);
+
+  @override
+  List<Object> get props => [data];
+}
