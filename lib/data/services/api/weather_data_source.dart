0e1fb10a85b32cac2e90f81a1a117573f8afeb92
teukumunawark <munawartmk@gmail.com>
2023-04-19 22:16:17 +0700

    update data source

--- lib/data/services/api/weather_data_source.dart
+++ lib/data/services/api/weather_data_source.dart
@@ -1,31 +1,29 @@
 import 'package:dio/dio.dart';
-import 'package:pawang_hujan/data/models/weathers.dart';
-import 'package:pawang_hujan/domain/exception/exception.dart';
+
+import '../../../domain/exception/exception.dart';
+import '../../models/weathers.dart';
 
 abstract class WeatherDataSource {
-  Future<Weathers> getWeatherApi();
+  Future<Weathers> getWeatherApi(String lang, double lat, double lon);
 }
 
 class WeatherDataSourceIMPL extends WeatherDataSource {
   static const apiURL = 'https://api.openweathermap.org/data/2.5/weather';
   static const appid = '1f2ac2140cc14ef9e6fa782d34018057';
 
-  static const lang = 'id';
-  static const lat = '5.522470';
-  static const lon = '95.410808';
-
   Dio dio = Dio();
 
   WeatherDataSourceIMPL({required this.dio});
 
   @override
-  Future<Weathers> getWeatherApi() async {
-    const url = "$apiURL?lat=$lat&lon=$lon&appid=$appid&lang=$lang";
-    Response response = await dio.get(url);
-    if (response.statusCode == 200) {
+  Future<Weathers> getWeatherApi(String lang, double lat, double lon) async {
+    try {
+      var url =
+          "$apiURL?lat=$lat&lon=$lon&appid=$appid&lang=$lang&units=metric";
+      Response response = await dio.get(url);
       return Weathers.fromJson(response.data);
-    } else {
-      throw ServerException();
+    } on DioError catch (e) {
+      throw ServerException(e.toString());
     }
   }
 }
